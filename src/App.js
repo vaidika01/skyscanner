aa8300f083da3f348f66dc73b160f6b77a22b382
vaidika01 <2022.vaidika@gmail.com>
2024-05-26 02:04:45 +0530

    change to week

--- src/App.js
+++ src/App.js
@@ -26,7 +26,42 @@ const daysOfWeek = [
     index: 0,
     isWeekend: true,
   },
-  // ...
+  {
+    name: "Monday",
+    nameAbbr: "Mon",
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: "Tuesday",
+    nameAbbr: "Tues",
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: "Wednesday",
+    nameAbbr: "Wed",
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: "Thursday",
+    nameAbbr: "Thur",
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: "Friday",
+    nameAbbr: "Fri",
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: "Saturday",
+    nameAbbr: "Sat",
+    index: 6,
+    isWeekend: true,
+  },
 ];
 
 export default class App extends Component {
